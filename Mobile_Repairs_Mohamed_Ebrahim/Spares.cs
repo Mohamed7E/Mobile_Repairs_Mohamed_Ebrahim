ac3008738e34200876fe872bc6dcc2a73d3a2e34
Mohamed7E <m.ibrahim.ict@btu.edu.eg>
2023-01-02 00:36:07 +0200

    Spares

---
+++ Mobile_Repairs_Mohamed_Ebrahim/Spares.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Mobile_Repairs_Mohamed_Ebrahim
+{
+    public partial class Spares : Form
+    {
+        public Spares()
+        {
+            InitializeComponent();
+        }
+    }
+}
